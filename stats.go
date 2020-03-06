c33b3e87f5c8ffed80b3e960ec5d156909698af0
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2020-03-06 11:52:08 +0900

    stats code for variance check

---
+++ stats.go
@@ -0,0 +1,16 @@
+package hdbscan
+
+import (
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
+)
+
+// GeneralizedVariance will return the determinant of the covariance matrix
+// of the supplied data.
+// The supplied data is a list of 'rows' observations of length 'columns'.
+func GeneralizedVariance(rows, columns int, data []float64) float64 {
+	covMatrix := &mat.SymDense{}
+	matrix := mat.NewDense(rows, columns, data)
+	stat.CovarianceMatrix(covMatrix, matrix, nil)
+	return mat.Det(covMatrix)
+}
