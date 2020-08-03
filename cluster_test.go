25a3a222c7458f5abf22fcd8e6afa5aa6147c507
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2020-08-02 20:50:15 -0500

    licensing fix

--- cluster_test.go
+++ cluster_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Hummility AI Incorporated, All Rights Reserved.
+// Copyright 2020 Humility AI Incorporated, All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
